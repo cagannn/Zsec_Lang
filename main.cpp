4cbc3973a984adc415e0c2fe772fafa3e7c74d2e
Çağan <cagangdk6@gmail.com>
2024-10-12 20:18:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,25 +5,42 @@
 #include "include/lexer.h"
 #include "include/syntax.h"
 
-
+bool end_with(std::string file_name){
+    std::string _end="";
+    for(int i=0;i<file_name.length();i++){
+    if(file_name[i]=='.'){
+        for (int j=i+1; j<file_name.length();j++){
+            _end=_end+file_name[j];
+        }
+        break;
+    }
+    }
+    if(_end=="zsec")
+        return true;
+    else
+        return false;
+}
 
 int main(int argc, char** argv){
+    if(end_with(argv[1]))
+    {
     int* array;
     std::string satir;
-
-    for (int i = 0; i < argc; ++i) {
-        std::cout << argv[i] << "\n";
-    }
-    
+    std::string file_name;
+    file_name=argv[1];  
     std::ifstream dosyaOku(argv[1], std::ios::in);
-            
-    if(dosyaOku.is_open()){
+   
+   if(dosyaOku.is_open()){
         while(getline(dosyaOku,satir)){
+            //x[i]=kar;
+            
             array=lexing(satir);
             syntaxer(satir,array, satir.length());
         }
        
         dosyaOku.close();
-    }
+    }}
+    else
+    std::cout<<"Error";
     return 0;
 }
